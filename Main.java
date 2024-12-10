c735e3fb7937c4a600221a0ee4f56cfcfa0d3ddd
Akib Miskat <akibmiskat@gmail.com>
2024-12-10 21:23:37 +0600

    Initial code

---
+++ Main.java
@@ -0,0 +1,11 @@
+package java_project.OOD;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Operation operlation = new Operation();
+        operlation.bankinfo();
+    }
+
+}
